3c43e119b0480aec3e23949df18b61bcaa90979b
Jackie <irene.yanapouth@gmail.com>
2023-05-28 10:08:36 +0200

    first

---
+++ main.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.btn');
+console.log(btn);
+
+const ctn = document.querySelector('.container-notifications');
+console.log(ctn);
+
+btn.addEventListener('click', function() {
+    console.log('bouton cliké');
+});
